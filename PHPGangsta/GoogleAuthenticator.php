465a424cc318617953b5712719a1b393b6108540
yanniks <kontakt@yanniks.de>
2015-03-28 17:00:15 -0400

    urlencode title Otherwise we might occur problems when using things like whitespaces.

--- PHPGangsta/GoogleAuthenticator.php
+++ PHPGangsta/GoogleAuthenticator.php
@@ -77,7 +77,7 @@ class PHPGangsta_GoogleAuthenticator
     public function getQRCodeGoogleUrl($name, $secret, $title = null) {
         $urlencoded = urlencode('otpauth://totp/'.$name.'?secret='.$secret.'');
 	if(isset($title)) {
-                $urlencoded .= urlencode('&issuer='.$title);
+                $urlencoded .= urlencode('&issuer='.urlencode($title));
         }
         return 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl='.$urlencoded.'';
     }
