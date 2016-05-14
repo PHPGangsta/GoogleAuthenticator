9b6726eae81b913bef63f8e70b2f3a1434d0ce3b
Łukasz Piechowiak <lukasz.piechowiak@gmail.com>
2016-05-14 17:20:53 +0200

    Adding parameters to the method 'getQRCodeGoogleUrl'

--- PHPGangsta/GoogleAuthenticator.php
+++ PHPGangsta/GoogleAuthenticator.php
@@ -72,14 +72,19 @@ class PHPGangsta_GoogleAuthenticator
      * @param string $name
      * @param string $secret
      * @param string $title
+     * @param array $params
      * @return string
      */
-    public function getQRCodeGoogleUrl($name, $secret, $title = null) {
+    public function getQRCodeGoogleUrl($name, $secret, $title = null, $params = []) {
+        $width = !empty($params['width'] && (int)$params['width'] > 0) ? (int)$params['width'] : 200;
+        $height = !empty($params['height'] && (int)$params['height'] > 0) ? (int)$params['height'] : 200;
+        $level = !empty($params['level'] && array_search($params['level'], ['L', 'M', 'Q', 'H']) !== false) ? $params['level'] : 'M';
+        
         $urlencoded = urlencode('otpauth://totp/'.$name.'?secret='.$secret.'');
 	if(isset($title)) {
                 $urlencoded .= urlencode('&issuer='.urlencode($title));
         }
-        return 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl='.$urlencoded.'';
+        return 'https://chart.googleapis.com/chart?chs='.$width.'x'.$height.'&chld='.$level.'|0&cht=qr&chl='.$urlencoded.'';
     }
 
     /**
