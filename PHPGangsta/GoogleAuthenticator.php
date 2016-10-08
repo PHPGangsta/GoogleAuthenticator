0ad4c4c6ef8e3ed14fbc2df5bcaf58b92da3ce27
Michael Kliewe <info@phpgangsta.de>
2016-10-08 18:18:54 +0200

    Use random_bytes() in PHP >=7.0 if available

--- PHPGangsta/GoogleAuthenticator.php
+++ PHPGangsta/GoogleAuthenticator.php
@@ -30,10 +30,11 @@ class PHPGangsta_GoogleAuthenticator
         }
         $secret = '';
         $rnd = false;
-        if (function_exists('mcrypt_create_iv')) {
+        if (function_exists('random_bytes')) {
+            $rnd = random_bytes($secretLength);
+        } elseif (function_exists('mcrypt_create_iv')) {
             $rnd = mcrypt_create_iv($secretLength, MCRYPT_DEV_URANDOM);
-        }
-        elseif (function_exists('openssl_random_pseudo_bytes')) {
+        } elseif (function_exists('openssl_random_pseudo_bytes')) {
             $rnd = openssl_random_pseudo_bytes($secretLength, $cryptoStrong);
             if (!$cryptoStrong) {
                 $rnd = false;
